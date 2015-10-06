b5d3d1cc21bda0742d7143670152391bdd853558
Lionel Lecaque <lionel@taotesting.com>
2015-10-06 11:01:05 +0000

    imporve error handling

--- src/oat-sa/deploymentsTools/src/Controller/DeployController.php
+++ src/oat-sa/deploymentsTools/src/Controller/DeployController.php
@@ -41,20 +41,20 @@ class DeployController extends AbstractActionController
         }
         $response =  is_file($filename) && $curl->response ? 'OK' : 'FAIL';
         
-
-        $tar = new \Archive_Tar($filename, "gz");
-        $tar->setErrorHandling(PEAR_ERROR_EXCEPTION);
-        $tar->extract($dataDir. 'extract');
-        
-        
-        $buildResult = $this
-            ->getServiceLocator()
-            ->get('BsbPhingService')
-            ->build('help', array(
-                'buildFile' => $dataDir. 'extract/' . $id . '/build.xml',
-                'propertyfile' =>  $dataDir . 'extract/' . $id . '/build.properties'
-            ));
-        
+        if(is_file($filename)) {
+            $tar = new \Archive_Tar($filename, "gz");
+            $tar->setErrorHandling(PEAR_ERROR_EXCEPTION);
+            $tar->extract($dataDir. 'extract');
+            
+            
+            $buildResult = $this
+                ->getServiceLocator()
+                ->get('BsbPhingService')
+                ->build('help', array(
+                    'buildFile' => $dataDir. 'extract/' . $id . '/build.xml',
+                    'propertyfile' =>  $dataDir . 'extract/' . $id . '/build.properties'
+                ));
+        }
         //var_dump($buildResult);
 /*         echo 'cmd'. PHP_EOL . $buildResult->getCommandLine();
          echo 'out' . PHP_EOL . $buildResult->getOutput();
@@ -66,7 +66,7 @@ class DeployController extends AbstractActionController
             'test' => $testParckageUrl,
             'id' => $id,
             'download' => $response,
-            'phingExitCode' => $buildResult->getExitCodeText(),
+            'phingExitCode' => isset($buildResult) ? $buildResult->getExitCodeText() : 'FAIL' ,
 
         ));
     }
